d5caa25d05b5924db016f70fe6cefef6d7b43aee
gilangfrc <gilangrachman10@gmail.com>
2022-11-28 11:24:58 +0700

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,13 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
       ),
       home: const MainPage(),
-    );
-  }
+    ),
+  );
 }
